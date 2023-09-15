7bdad57eb7cff60f06bd5d71e2983dcbc3e4dfce
alex-dsci <alex-dsci@users.noreply.github.com>
2023-09-15 18:48:59 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="alex-dsci"
+original_name="idr_design"
+original_urlname="IDR-design"
+original_description="Awesome idr_design created by alex-dsci"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv idr_design $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ idr_design/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for idr_design."""
+
+from idr_design.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
