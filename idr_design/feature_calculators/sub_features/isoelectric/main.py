dfbc56ee142f3b6424f8c85acd086aebb2caec4e
alex-dsci <alex-dsci@github.com>
2023-09-19 18:09:08 -0400

    Refactored isoelectric point constants

--- idr_design/feature_calculators/sub_features/isoelectric/main.py
+++ idr_design/feature_calculators/sub_features/isoelectric/main.py
@@ -19,27 +19,29 @@ PKA_C_TERM: float = 3.55
 PKAS_ALL: dict[str, float] = PKAS_BASIC.copy() 
 PKAS_ALL.update(PKAS_ACIDIC)
 ACID_BASE_RES: list[str] = list(PKAS_ALL.keys())
-KAS_ALL: list = list(map(lambda x: 10 ** (-x), PKAS_ALL.values()))
 MAX_TIME_SECONDS: int = 10
 
-@timeout(MAX_TIME_SECONDS, "pI calculation timed out.")
-def get_pI(seq_or_counts: str | dict[str, int]) -> float:
-    counts: list[int]
-    N_basic: int
+
+def handle_pI(seq_or_counts: str | dict[str, int]) -> float:
+    counts: dict[str, int]
     if isinstance(seq_or_counts, str):
-        counts = list(map(lambda res: seq_or_counts.count(res), ACID_BASE_RES)) 
-        N_basic: int = sum([seq_or_counts.count(res) for res in BASIC_RES])
+        counts = dict(map(lambda res: (res, seq_or_counts.count(res)), ACID_BASE_RES)) 
     else:
-        counts = list(map(lambda res: seq_or_counts[res], ACID_BASE_RES)) 
-        N_basic: int = sum([seq_or_counts[res] for res in BASIC_RES])
-    to_optimize: func[[float], float] = lambda pH: charge_at_pH(pH, N_basic, zip(counts, KAS_ALL))
+        counts = seq_or_counts
+    return _pI(counts)
+
+@timeout(MAX_TIME_SECONDS, "pI calculation timed out.")
+def _pI(all_counts: dict[str, int]) -> float:
+    counts = list(map(lambda res: all_counts[res], ACID_BASE_RES)) 
+    N_basic: int = sum([all_counts[res] for res in BASIC_RES])
+    to_optimize: func[[float], float] = lambda pH: _charge_at_pH(pH, N_basic, zip(counts, PKAS_ALL.values()))
     scipy_root: float = root_scalar(to_optimize, method="brenth", bracket=(0,14)).root
     return scipy_root
 
-def charge_at_pH(pH: float, num_basic_res: int, counts_and_KAs: Iterator[tuple[int, float]]) -> float:
+def _charge_at_pH(pH: float, num_basic_res: int, counts_and_pKAs: Iterator[tuple[int, float]]) -> float:
     free_protons: float = 0
-    for count, Ka in counts_and_KAs:
-        proportion_protonated = 1 / (1 + Ka * (10 ** pH))
+    for count, pKA in counts_and_pKAs:
+        proportion_protonated = 1 / (1 + 10 ** (pH - pKA))
         free_protons += count * (1 - proportion_protonated)
     # N terminus
     proportion_protonated = 1 / (1 + (10 ** (pH - PKA_N_TERM)))
