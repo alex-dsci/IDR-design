3ce42fa6b3002a86a22ddbf0ab500e63b64cb179
alex-dsci <alex-dsci@github.com>
2023-10-07 13:48:51 -0400

    Added overwrites option for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,29 @@
 # Docs: https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option
 
 import pytest
+from itertools import product
 
 def pytest_addoption(parser):
     parser.addoption(
         "--run_slow", action="store_true", default=False, help="Runs slow tests."
     )
+    parser.addoption(
+        "--rerun_files", action="store_true", default=False, help="Runs tests that overwrite large files."
+    )
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "overwrites: mark test as slow and overwriting files")
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--run_slow"):
-        return
-    skip_slow = pytest.mark.skip(reason="Run with --run_slow")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+    skips = {}
+    if not config.getoption("--run_slow"):
+        skips.update({"slow": pytest.mark.skip(reason="Run with --run_slow")})
+    if not config.getoption("--rerun_files"):
+        skips.update({"overwrites": pytest.mark.skip(reason="Run with --rerun_files")})
+    for item, reason in product(items, skips.keys()):
+        if reason in item.keywords:
+            item.add_marker(skips[reason])
 
 # # each test runs on cwd to its temp dir
 # @pytest.fixture(autouse=True)
